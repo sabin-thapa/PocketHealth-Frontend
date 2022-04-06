2a1209e46165561f035ce109c79cd24e3fad89cd
Sabin Thapa <sabint017@gmail.com>
2022-04-06 23:28:35 +0545

    add signup role: patient + practitioner

--- navigation/AuthNavigator.jsx
+++ navigation/AuthNavigator.jsx
@@ -7,12 +7,14 @@ import SignIn from "../screens/SignIn";
 import SignUp from "../screens/SignUp";
 import DrawerNavigator from "./Drawer/DrawerNavigator";
 import SignUpDatabase from "../screens/SignUpDatabase";
+import SignUpRole from "../screens/SignUpRole";
 
 const Stack = createStackNavigator();
 const AuthNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Home" component={Carousal} />
+      <Stack.Screen name="SignUpRole" component={SignUpRole} />
       <Stack.Screen name="SignIn" component={SignIn} />
       <Stack.Screen name="SignUp" component={SignUp} />
       <Stack.Screen name="SignUpDatabase" component={SignUpDatabase} />
